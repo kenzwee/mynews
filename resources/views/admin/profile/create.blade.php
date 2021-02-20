5133c153e962a4d6e372d20bb7046a2087a633e9
kenji <ostardevaken@yahoo.co.jp>
2021-02-20 07:41:53 +0000

    laravel11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" vontent="width=device-width, initial-scale=1">
-        <title>MyProfile</title>
-    </head>
-    <body>
-        <h1>Myプロフィール作成画面</h1>
-    </body>
-</html>
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- layouts/profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
